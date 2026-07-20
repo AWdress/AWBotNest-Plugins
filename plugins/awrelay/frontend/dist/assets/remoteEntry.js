208a77a5c750841ecc178c9ee5ad96d2b6b7cb2f
AWdress <awdress.lt@gmail.com>
2026-07-20 09:59:11 +0800

    修复 AWRelay 话题列表拉黑操作

--- plugins/awrelay/frontend/dist/assets/remoteEntry.js
+++ plugins/awrelay/frontend/dist/assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./Config":()=>{
-      dynamicLoadingCss(["style-BQGQoEKX.css"], false, './Config');
-      return __federation_import('./__federation_expose_Config-DCdIt9_K.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-DclUONCW.css"], false, './Config');
+      return __federation_import('./__federation_expose_Config-DU-zHYli.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
