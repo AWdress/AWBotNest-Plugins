69905038b99611de871c33a34f6bb3407962bf86
AWdress <awdress.lt@gmail.com>
2026-07-20 12:24:54 +0800

    补充自动订阅插件 Logo

--- plugins/auto_subscribe/frontend/dist/assets/index-CR6yQij9.js
+++ plugins/auto_subscribe/frontend/dist/assets/index-CR6yQij9.js
@@ -1,5 +1,5 @@
 import { importShared } from './__federation_fn_import-JrT3xvdd.js';
-import Config from './__federation_expose_Config-DyESZcbQ.js';
+import Config from './__federation_expose_Config-btsXdASn.js';
 
 true              &&(function polyfill() {
   const relList = document.createElement("link").relList;
