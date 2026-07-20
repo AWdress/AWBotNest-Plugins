89b114537abed775a5e31425b6bda5c426d7ec86
AWdress <awdress.lt@gmail.com>
2026-07-20 08:10:14 +0800

    AWRelay 改用随机人机验证题

--- plugins/awrelay/frontend/dist/assets/index-DiXV-Jn6.js
+++ plugins/awrelay/frontend/dist/assets/index-DiXV-Jn6.js
@@ -1,5 +1,5 @@
 import { importShared } from './__federation_fn_import-GzAXfPDJ.js';
-import Config from './__federation_expose_Config-Bu3_mHG4.js';
+import Config from './__federation_expose_Config-UAuiQxPM.js';
 
 true              &&(function polyfill() {
   const relList = document.createElement("link").relList;
