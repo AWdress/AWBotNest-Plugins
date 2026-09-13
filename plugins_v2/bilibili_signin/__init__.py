f1698da21aecf6a856153a5c54d6a041d0d9079e
AWdress <awdress.lt@gmail.com>
2026-09-13 10:45:48 +0800

    Revert "fix(signin): add reliable Vue config persistence"
    This reverts commit cf4f1bb8c08d273c1621471b39aa0c911868eb4f.

--- plugins_v2/bilibili_signin/__init__.py
+++ plugins_v2/bilibili_signin/__init__.py
@@ -23,7 +23,6 @@ __plugin__ = {
     "plugin_api_version": 2,
     "tags": ["B站", "每日签到", "Cookie"],
     "default_enabled": False,
-    "render_mode": "vue",
     "requirements": ["requests>=2.28"],
     "resources": {"timeout_seconds": 600, "max_concurrency": 1, "max_background_tasks": 2},
     "config_schema": {
